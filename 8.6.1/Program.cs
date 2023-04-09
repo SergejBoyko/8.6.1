48b2ac9e4bd8f4fb16c1addfd7648ed82ed0c5c3
SergejBoyko <126371372+SergejBoyko@users.noreply.github.com>
2023-04-09 16:58:28 +0700

    Update Program.cs

--- 8.6.1/Program.cs
+++ 8.6.1/Program.cs
@@ -15,8 +15,12 @@ namespace DirectoryManager
             try
             {
                 string dirName = @"C:\Users\B1S3\Desktop\testFolder12"; // Прописываем путь к корневой директории
+                long a = GetSizeFolder(dirName);
+                Console.WriteLine("Текущий размер каталога {0} байт",a);
                 if (Directory.Exists(dirName)) // Проверим, что директория существует
-                    GetFiles(dirName);
+                   GetFiles(dirName);
+                    
+
                 {
                     string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
 
@@ -32,6 +36,9 @@ namespace DirectoryManager
                         }
                     }
                 }
+                long b = GetSizeFolder(dirName);
+                Console.WriteLine("Особождено {0}",a - b);
+                Console.WriteLine("Текущий размер каталога {0} байт", b);
             }
             catch (Exception ex)
             {
@@ -52,13 +59,30 @@ namespace DirectoryManager
         {
             DateTime date2 = File.GetLastAccessTime(s);
             DateTime date1 = DateTime.Now;
-            TimeSpan timeSpan = TimeSpan.FromMinutes(30);
+            TimeSpan timeSpan = TimeSpan.FromMinutes(20);
             TimeSpan timeSpan1 = date1 - date2;
             if (timeSpan1 > timeSpan)
             {
                 Console.WriteLine("Файл {0} удален, изменения были более 30 минут назад", s);
                 File.Delete(s);
             }
+        }
+        public static long GetSizeFolder(string dirName)
+        {
+            long sizeFiles = 0;
+            int files = 0;
+            string[] fis = Directory.GetFiles(dirName);
+            foreach (string fi in fis) 
+            {
+                sizeFiles += fi.Length;
+                files++;
+            }
+            string[] dir = Directory.GetDirectories(dirName);
+            foreach (var di in dir)
+            {
+                sizeFiles += GetSizeFolder(di);
+            }
+            return sizeFiles;
         }
     }
 }
